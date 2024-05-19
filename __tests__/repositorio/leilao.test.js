4c97f36239993bf2ab9456d9937b46eb8539f47f
Luiz Almada <luizalmada@yahoo.com>
2024-05-19 13:44:42 -0300

    Final

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -1,15 +1,14 @@
-import { obtemLeiloes } from "../../src/repositorio/leilao";
-import apiLeiloes from "../../src/servicos/apiLeiloes";
-import React from 'react';
+import { obtemLeiloes } from '../../src/repositorio/leilao';
+import apiLeiloes from '../../src/servicos/apiLeiloes';
 
-jest.mock("../../src/servicos/apiLeiloes");
+jest.mock('../../src/servicos/apiLeiloes');
 
 const mockLeiloes = [
   {
     id: 1,
-    nome: "Leilão",
-    descricao: "Descrição do leilão",
-  },
+    nome: 'Leilão',
+    descricao: 'Descrição do leilão'
+  }
 ];
 
 const mockRequisicao = (retorno) => {
@@ -23,40 +22,43 @@ const mockRequisicao = (retorno) => {
 }
 
 const mockRequisicaoErro = () => {
-  return new Promise((_,reject) => {
+  return new Promise((_, reject) => {
     setTimeout(() => {
       reject();
     }, 200);
-  })
+  });
 }
 
-describe("repositorio/leilao", () => {
+describe('respositorio/leilao', () => {
 
-  // Limpa o mock
   beforeEach(() => {
     apiLeiloes.get.mockClear();
-  })
+  });
 
-  describe("obtemLeiloes", () => {
-    it("deve retornar um lista de leilões", async () => {
-      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes));
+  describe('obtemLeiloes', () => {
+
+    it('deve retornar uma lista de leilões', async () => {
+      apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
+      
       const leiloes = await obtemLeiloes();
+
       expect(leiloes).toEqual(mockLeiloes);
+
       expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
       expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
     });
 
-    it("deve retornar um lista vazia quando a requisição falhar", async () => {
+    it('deve retornar uma lista vazia quando a requisição falhar', async () => {
       apiLeiloes.get.mockImplementation(() => mockRequisicaoErro());
+      
       const leiloes = await obtemLeiloes();
+
       expect(leiloes).toEqual([]);
+
       expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
-      expect(apiLeiloes.get).toHaveBeenCalledTimes(1); 
+      expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
     });
 
   });
 
-  test('testa o mock do método useEffect', () => {
-      jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
-  });
 });
--- src/servicos/apiLeiloes.js
+++ src/servicos/apiLeiloes.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// create axios
+// Use seu IP local
 const instance = axios.create({
-  baseURL: 'http://192.168.1.10:3000/',
+  baseURL: 'http://192.168.1.14:3000/',
   timeout: 1000,
 });
 
